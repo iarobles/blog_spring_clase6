70d24e4e7d849afc2d0c501d7d6e778202548552
Ismael Robles <ismael_ariel@hotmail.com>
2014-07-12 10:26:37 -0500

    subo blog primera vez

---
+++ src/main/java/org/upiita/spring/seguridad/LoginFallidoManejador.java
@@ -0,0 +1,38 @@
+package org.upiita.spring.seguridad;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.web.authentication.SimpleUrlAuthenticationFailureHandler;
+
+public class LoginFallidoManejador extends SimpleUrlAuthenticationFailureHandler{
+	
+	
+	@Override
+	public void onAuthenticationFailure(HttpServletRequest request,
+			HttpServletResponse response, AuthenticationException exception)
+			throws IOException, ServletException {
+		
+		// TODO Auto-generated method stub
+		
+		Integer loginsFallidos = (Integer) request.getSession().getAttribute("loginsFallidos");
+		
+		//SI ES LA PRIMERA VEZ QUE SE EQUIVO EL USUARIO
+		if(loginsFallidos == null){
+			loginsFallidos = 1;
+		} else {
+			//SI YA EXISTE LA VARIABLE EN LA SESION
+			loginsFallidos++;
+		}
+		
+		request.getSession().setAttribute("loginsFallidos", loginsFallidos);
+		
+		
+		super.onAuthenticationFailure(request, response, exception);
+	}
+
+}
